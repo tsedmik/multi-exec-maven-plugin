daf1bc86987c75fce7c0ee00ca937ed1b4b50cbd
Tomáš Sedmík <tsedmik@redhat.com>
2017-10-19 11:10:39 +0200

    Extend tests (single and multiple location for archivation)

--- src/test/java/org/jboss/tsedmik/multi_exec_maven_plugin/RunMojoTest.java
+++ src/test/java/org/jboss/tsedmik/multi_exec_maven_plugin/RunMojoTest.java
@@ -13,7 +13,7 @@ public class RunMojoTest extends AbstractMojoTestCase {
 	/**
 	 * 1. test presence of 'run' goal
 	 * 2. test execution of 'run' goal
-	 * 3. test archivation
+	 * 3. test archivation (multiple location)
 	 * 
 	 * @throws Exception if something went wrong
 	 */
@@ -28,7 +28,33 @@ public class RunMojoTest extends AbstractMojoTestCase {
 		}
 		File archivedFile = new File (getBasedir(), "target/test-archive/RunMojo.java");
 		assertNotNull(archivedFile);
-		archivedFile = new File (getBasedir(), "target/test-archive/RunMojoTest.java");
 		assertTrue(archivedFile.exists());
+		archivedFile = new File (getBasedir(), "target/test-archive/RunMojoTest.java");
+		assertNotNull(archivedFile);
+		assertTrue(archivedFile.exists());
+	}
+
+	/**
+	 * 1. test presence of 'run' goal
+	 * 2. test execution of 'run' goal
+	 * 3. test archivation (single location)
+	 * 
+	 * @throws Exception if something went wrong
+	 */
+	public void testMojoGoal2() throws Exception {
+		File testPom = new File(getBasedir(), "src/test/resources/test-pom/pom2.xml");
+		RunMojo mojo = (RunMojo) lookupMojo("run", testPom);
+		assertNotNull(mojo);
+		try {
+			mojo.execute();
+		} catch (Exception e) {
+			fail("Something is wrong with test build!");
+		}
+		File archivedFile = new File (getBasedir(), "target/test-archive2/RunMojo.java");
+		assertNotNull(archivedFile);
+		assertTrue(archivedFile.exists());
+		archivedFile = new File (getBasedir(), "target/test-archive2/RunMojoTest.java");
+		assertNotNull(archivedFile);
+		assertFalse(archivedFile.exists());
 	}
 }
